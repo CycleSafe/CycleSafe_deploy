121d1558be66530b86d4f3cf4e448fdd9317c730
Ghaida Zahran <ghaida@zurb.com>
2014-08-28 19:40:41 -0700

    Adds css and js files

---
+++ app/static/js/app.js
@@ -0,0 +1,5 @@
+$(document).foundation();
+
+$( "#datepicker" ).datepicker({
+  inline: true
+});
---
+++ app/static/js/maps.js
@@ -0,0 +1,60 @@
+// This was a homework assignment in the first few weeks of my bootcamp. It shows local weather on a map using GeoLocation (with HTML and libraries needed).
+
+$(document).ready(function(){ 
+
+	initLocationControls();
+
+	addMarkers();
+});
+
+function initLocationControls(){
+	var geoMessage;
+
+	if (Modernizr.geolocation){
+		navigator.geolocation.getCurrentPosition(locationData, locationError);
+	} else {
+		return false;
+	}
+
+}
+
+//If geolocation doesn't work, run an error message.
+function locationError(){
+	alert('Please enable geolocation to continue.');
+	return false;
+}
+
+//Collects local data and weather data. 
+
+function locationData(position){
+	console.log(position);	
+	//Sets variables we'll need to generate other fields.
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
+	var timestamp = position.coords.timestamp;
+
+    //Set multiple values.
+    document.getElementById('#id_address').setAttribute("value", (lat + ',' + lon));
+
+    mapGenerator(lat, lon);
+
+}
+
+function mapGenerator(lat, lon){
+	var mapOptions = {
+		center: new google.maps.LatLng(lat, lon),
+		zoom: 12,
+		mapTypeId: google.maps.MapTypeId.ROAD
+
+	};
+	var map = new google.maps.Map(
+		document.getElementById("map-canvas"),
+		mapOptions);
+
+
+//Need to pull marker data and set it to something.
+//google.maps.event.addListener(marker, 'click', function() {
+//	infowindow.open(map,marker)
+//});
+//
+}
