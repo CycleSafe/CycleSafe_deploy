676945925ec61456dea7540d527350383256c105
cjonagam <cjonagam@equinix.com>
2014-11-10 22:58:00 -0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+from setuptools import setup, find_packages
+ 
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+ 
+setup(name='paasbakeoff',
+    version='1.0',
+    author='Nate Aune',
+    author_email='nate@appsembler.com',
+    url='https://github.com/appsembler/paasbakeoff',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Example Mezzanine CMS deploy to OpenShift PaaS',
+    install_requires=open('%s/cyclesafe/requirements.txt' % os.environ.get('OPENSHIFT_REPO_DIR', PROJECT_ROOT)).readlines(),
+)
