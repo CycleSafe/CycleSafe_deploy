85a65037744121038ca69accdce453c7bad96dd7
Zhila <zhila@Zhilas-MacBook-Pro.local>
2015-01-03 17:02:49 -0800

    Revert "added UI buttons to form"
    This reverts commit ac4de097c99cae7fe6f1dfc996be9fd7e27e2f64.

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,9 @@
 from app.models import Hazard
 from django.forms import ModelForm, DateTimeInput
-from django.forms.widgets import RadioSelect
-
 
 class HazardForm(ModelForm):
     class Meta:
         model = Hazard
-        widgets = {'date_time': DateTimeInput(attrs={'class': 'datepicker'}),
-                   'user_type': RadioSelect(attrs={'id': 'value', 'class': 'hidden inline-block'})}
+        widgets = {'date_time': DateTimeInput(attrs={'class': 'datepicker'})}
         # TODO(zemadi): Assign someone to edit ranking field once it's implemented.
         exclude = ('ranking',)
