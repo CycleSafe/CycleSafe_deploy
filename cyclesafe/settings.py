e1b351250302cf95ebd9049ffcc77c59bfc0f8ed
Zhila <zfemadi@gmail.com>
2015-02-19 21:14:33 -0800

    first commit to new master

--- cyclesafe/settings.py
+++ cyclesafe/settings.py
@@ -32,8 +32,8 @@ ROOT_PATH = os.path.dirname(__file__)
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Turned off debugging.
-DEBUG = True
-TEMPLATE_DEBUG = True
+DEBUG = False
+TEMPLATE_DEBUG = False
 
 # Application definition
 INSTALLED_APPS = (
