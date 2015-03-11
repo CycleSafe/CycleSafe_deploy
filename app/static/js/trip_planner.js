fc275d5a40b40cc2b58cbfa2f6ae79653b81e3b4
Zhila <zfemadi@gmail.com>
2015-03-10 22:18:45 -0700

    Trip planner not active but more JS changes

--- app/static/js/trip_planner.js
+++ app/static/js/trip_planner.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
     $('.active').toggleClass('active');
     $('#trip-planner').toggleClass('active');
     //TODO(zemadi): Remove two lines below.
-    $('#start').val('1901 rock st mountain view');
+    $('#start').val('1900 rock st mountain view');
     $('#end').val('725 san antonio palo alto');
 });
 
@@ -30,9 +30,10 @@ function setDirectionsDisplay(map) {
     directionsDisplay.setPanel(document.getElementById('directions-panel'));
 }
 
+// Listener for if the mode of travel is changed from an empty default to either bicycling or walking.
 function formListener() {
     $('#mode_travel').change(function(){
-       calcRoute();
+        calcRoute();
     });
 }
 
@@ -101,69 +102,71 @@ function getMarkers(response){
 function narrowResults(queryResults, directionsResponse) {
     // RouteBoxer chops up the user's route into sections.
     // Refer to: http://google-maps-utility-library-v3.googlecode.com/svn/trunk/routeboxer/docs/examples.html
-    var distance = .04;
     var path = directionsResponse.routes[0].overview_path;
-    var boxes = rboxer.box(path, distance);
+    var boxes = rboxer.box(path, .04);
     var mapData = {'objects': []};
 
+    console.log(boxes);
+    console.log(directionsResponse);
     // TODO(zemadi): Improve this. See if there's a way to skip routeBoxer. Avoid nested loop if possible.
+    // In order for a data point to be on the route, the coordinates
+    // need to be between the values of a box in routeBoxer.
+    // RouteBoxer chops up a route into sections and returns the upper and lower
+    // boundaries for that section of the route.
+
+    var routeByBounds = {};
+    var boundsWithData = {};
+    var steps = directionsResponse.routes[0].legs[0].steps;
+
+    // go through each step and set of lat lngs per step.
     for (var i = 0; i < boxes.length; i++) {
-        var sectionBounds = getBounds(boxes[i]);
-
-        // In order for a data point to be on the route, the coordinates
-        // need to be between the values of a box in routeBoxer.
-        // RouteBoxer chops up a route into sections and returns the upper and lower
-        // boundaries for that section of the route.
-
-        for (var j = 0; j < queryResults.length; j++) {
-            var currentLat = parseFloat(queryResults[j].lat);
-            var currentLon = parseFloat(queryResults[j].lon);
-            var latArraySort = [currentLat, sectionBounds.lat1, sectionBounds.lat2].sort();
-            var lonArraySort = [currentLon, sectionBounds.lon1, sectionBounds.lon2].sort();
-
-            // If a marker is between the latitude and longitude bounds of the route (index 1), add
-            // it to the map.
-            if (latArraySort.indexOf(currentLat) === 1) {
-                if (lonArraySort.indexOf(currentLon) === 1) {
-                    mapData.objects.push(queryResults[j]);
-
-                    // Build the text for the warning message.
-                    // Add a description to the warning message.
-                    if (queryResults[j].description) {
-                        var more_information = 'Details: ' + queryResults[j].description;
-                        directionsResponse.routes[0].warnings.unshift(more_information);
-                    }
-                    // Add the hazard type and address.
-                    var address = coordsToAddress([queryResults[j].lat, queryResults[j].lon]);
-                    var partialAddress = [address.results[0].address_components[0].short_name, address.results[0].address_components[1].short_name];
-                    var markerWarning = [queryResults[j].hazard_type, 'near', partialAddress.join(' ')];
-                    directionsResponse.routes[0].warnings.unshift(markerWarning.join(' '));
-                }
+        routeByBounds[boxes[i]] = [];
+        for (var j = 0; j < steps.length; j++) {
+            //TODO(zemadi): Test this more. Is end location enough?
+            if (boxes[i].contains(steps[j].end_location)) {
+                routeByBounds[boxes[i]].push(steps[j]);
+            }
+//            for (var k = 0; k < steps[j].lat_lngs.length; k++) {
+//                // If the lat and long of that path section falls in a routeBoxer bound, add it to the object.
+//                if (boxes[i].contains(steps[j].lat_lngs[k])) {
+//                    routeByBounds[boxes[i]].push(steps[j]);
+//                }
+//            }
+        }
+        for (var l = 0; l < queryResults.length; l++) {
+            // If a marker is between the latitude and longitude bounds of the route, add it to the map.
+            var currentResult = new google.maps.LatLng(queryResults[l].lat, queryResults[l].lon);
+            if (boxes[i].contains(currentResult)) {
+                mapData.objects.push(queryResults[l]);
+                boundsWithData[boxes[i]] = queryResults[j];
             }
         }
     }
-
-    // Add general hazard info to the warnings section.
-    // TODO(zemadi): Make custom directions panel so it combines marker and directions data.
-    var warningText = [];
-    if (mapData.length === 1) {
-        Array.prototype.push.apply(warningText, ['There is', mapData.objects.length,  'hazard along your route.']);
-    } else {
-        Array.prototype.push.apply(warningText, ['There are', mapData.objects.length, 'hazards along your route.']);
+    // Build out the custom directions panel and add marker data to the path as key-value. Build custom directions
+    // panel using only the directionsResponse data.
+    console.log('test');
+    //TODO(zemadi): Fix this so it loops through each key and looks for values.
+    for (var m = 0; m < routeByBounds.length; m++) {
+        console.log(routeByBounds[m]);
+        if (routeByBounds[m].length > 0) {
+            console.log(routeByBounds[m]);
+        }
     }
-    directionsResponse.routes[0].warnings.unshift(warningText.join(' '));
+  //  console.log(boundsWithData);
+
     // Remove existing markers from the map and map object.
     removeMarkers(true);
 
     if (mapData.objects.length > 0) {
         // Add new markers to the map.
+        console.log(mapData.objects);
         markerGenerator(map, mapData);
     }
 }
 
-// Function to get coordinate boundaries from the Directions route callback and RouteBoxer.
+// Function to get coordinate boundaries from the Directions route callback.
 // They both hold boundary data in a similar format.
-function getBounds(data){
+function getBounds(data) {
     var coordinateBounds = {};
     var keyByIndex = Object.keys(data);
 
@@ -174,3 +177,45 @@ function getBounds(data){
 
     return coordinateBounds;
 }
+
+//// Convert RouteBoxer's bounds object to a dict mapping each bound to a step on the directions path.
+//function convertBounds(boxes, directionsResponse){
+//    var routeByBounds = {};
+//    var boundsWithData = {};
+//    var steps = directionsResponse.routes[0].legs[0].steps;
+//
+//    // go through each step and set of lat lngs per step.
+//
+//    for (var i = 0; i < boxes.length; i++) {
+//        for (var j = 0; j < steps.length; j++) {
+//            for (var k = 0; k < steps[j].lat_lngs.length; k++) {
+//                var currentResult = new google.maps.LatLng(steps[j].lat_lngs[k]['D'], steps[j].lat_lngs[k]['k']);
+//                if (boxes[i].contains(currentResult)) {
+//                    routeByBounds[boxes[i]] = steps[j];
+//                    break;
+//                }
+//            }
+//        }
+//        for (var l = 0; l < queryResults.length; l++) {
+//
+//            // If a marker is between the latitude and longitude bounds of the route, add it to the map.
+//            var currentResult = new google.maps.LatLng(queryResults[l].lat, queryResults[l].lon);
+//            if (boxes[i].contains(currentResult)) {
+//                mapData.objects.push(queryResults[l]);
+//                boundsWithData[boxes[i]] = queryResults[j];
+//            }
+//        }
+//    }
+//    // Build out the custom directions panel and add marker data to the path as key-value. Build custom directions
+//    // panel using only the directionsResponse data.
+//
+//    return routebyBounds;
+//
+//
+//}
+//
+//// Function to build the custom directions panel, which holds directions and marker data.
+//// TODO(zemadi) Determine if this is needed. Build this and convert to handlebars template or React?
+//function buildDirectionsPanel(mapData, directionsResponse) {
+//    // For now, do nothing.
+//}
