e5c4a5931ece29f6c22b66a1ef3c539c4b13856f
Zhila <zfemadi@gmail.com>
2015-04-09 22:48:02 -0700

    cyclesafe-main

--- app/static/js/trip_planner.js
+++ app/static/js/trip_planner.js
@@ -4,6 +4,16 @@ var map;
 //var markers = [];
 var travelMode;
 
+// Order of operations for the trip planner (each function will have detailed notes):
+// 1) Calculate a user's route, with directions.
+// 2) Run a query using the route's max and min bounds to narrow potential results.
+// 3) Just because a marker is in the area of the route, it doesn't mean that it's on the route.
+//    Use RouteBoxer to map sections of the route to markers, if applicable. Display only those markers on the map.
+// 4) The directions panel also isn't linked to a section of the route.
+//    Use RouteBoxer to map a direction (turn left, right, etc) to part of the route.
+// 5) Build the custom directions panel by looping though each leg of the trip, finding the corresponding RouteBoxer
+//    section, and use that to get markers for that section only.
+
 $(document).ready(function () {
     $('.active').toggleClass('active');
     $('#trip-planner').toggleClass('active');
@@ -118,8 +128,8 @@ function getBounds(data) {
 
 
 // Reduce the query results further by checking if they're actually along a route.
-
 // In order for a data point to be on the route, the coordinates need to be between the values of a box in routeBoxer.
+
 // RouteBoxer chops up a route into sections and returns the upper and lower boundaries for that section of the route.
 // Refer to: http://google-maps-utility-library-v3.googlecode.com/svn/trunk/routeboxer/docs/examples.html
 function narrowResults(queryResults, directionsResponse) {
@@ -130,17 +140,21 @@ function narrowResults(queryResults, directionsResponse) {
 
     //Variables to hold mapData and match a marker to a specific section of the route.
     var mapData = {'objects': []};
-    var markerBoxes = {};
+    // Object to hold routeBoxer index to markers map.
+    var mapBoxesAndMarkers = {};
 
     // For each section of the route, look through markers to see if any fit in the section's boundaries.
+    // Using a for loop here because routeBoxer returns an array.
     for (var j = 0; j < boxes.length; j++) {
-        markerBoxes[j] = [];
+        // For each section of the route, record the index as a key and create an empty array to hold marker values.
+        mapBoxesAndMarkers[j] = [];
         queryResults.forEach(function(result) {
-            // If a marker is between the latitude and longitude bounds of the route, add it to the map.
+            // If a marker is between the latitude and longitude bounds of the route, add it to the map and
+            // the route-marker dict.
             var currentResult = new google.maps.LatLng(result.lat, result.lon);
             if (boxes[j].contains(currentResult)) {
                 mapData.objects.push(result);
-                markerBoxes[j].push(result);
+                mapBoxesAndMarkers[j].push(result);
             }
         });
     }
@@ -155,53 +169,51 @@ function narrowResults(queryResults, directionsResponse) {
         directionsResponse.routes[0].legs[0]['marker_count'] = 'None';
     }
 
-    mapDirectionsToBoxes(boxes, directionsResponse, markerBoxes);
+    mapDirectionsToBoxes(boxes, directionsResponse, mapBoxesAndMarkers);
 
 }
 
-// Directions information also needs to be matched to a section of the route.
-function mapDirectionsToBoxes(boxes, directionsResponse, markerBoxes){
-    var directionsWithBoxes = {};
-    var steps = directionsResponse.routes[0].legs[0].steps;
+// Directions information also needs to be mapped to a section of the route.
+function mapDirectionsToBoxes(boxes, directionsResponse, mapBoxesAndMarkers){
+    var directions = directionsResponse.routes[0].legs[0].steps;
 
     // go through each step and set of lat lngs per step.
-    for (var k = 0; k < steps.length; k++) {
-        directionsWithBoxes[k] = [];
-        steps[k]['markers'] = [];
+     directions.forEach(function(direction) {
+        var routeBoxesinDirection = [];
         for (var l = 0; l < boxes.length; l++) {
-            steps[k].lat_lngs.forEach(function(lat_lng) {
-                if (directionsWithBoxes[k].indexOf(l) === -1 && boxes[l].contains(lat_lng)) {
-                    directionsWithBoxes[k].push(l);
-                    // Move on to the next iteration of the loop.
-                    return;
+            direction.lat_lngs.forEach(function(lat_lng) {
+                // If the index isn't already in the array and the box contains the current route's lat and long, add the
+                // index.
+                if (routeBoxesinDirection.indexOf(l) === -1 && boxes[l].contains(lat_lng)) {
+                    routeBoxesinDirection.push(l);
                 }
             });
         }
 
-        var firstNumberInRange = directionsWithBoxes[k][0];
-        var lastNumberInRange = directionsWithBoxes[k].slice(-1)[0];
-        // For the current direction, look up the bound object that it's in. Use that to search for markers.
-        for (var m = firstNumberInRange; m <= lastNumberInRange; m++) {
-            if (markerBoxes[m].length > 0) {
-                steps[k]['markers'].push.apply(steps[k]['markers'], markerBoxes[m]);
+        // Once we're done looping over route boxes for the current direction, lookup markers that have the same bounds.
+        // A direction can have multiple route boxes so a list is being used here.
+        direction['markers'] = [];
+        routeBoxesinDirection.forEach(function(box) {
+            if (mapBoxesAndMarkers[box].length > 0) {
+                // Use the route box to look up arrays of markers to add to the directions object.
+                direction['markers'].push.apply(direction['markers'], mapBoxesAndMarkers[box]);
             }
-        }
-    }
+        });
+    });
 
    generateDirectionsPanel(directionsResponse);
     
 }
 
 function generateDirectionsPanel(directionsResponse) {
-    //TODO(zemadi): Precompile template? Better for mobile, supposedly.
     var directionsPanelHtml = $('#custom-directions-panel').html();
     var template = Handlebars.compile(directionsPanelHtml);
     var compiledDirectionsPanel = template(directionsResponse.routes[0].legs[0]);
 
-    var existingDirectionsPanel = $('#directions-panel');
-    if (existingDirectionsPanel.length) {
-        existingDirectionsPanel.replaceWith(compiledDirectionsPanel);
+    var directionsPanel = $('#directions-panel');
+    if (directionsPanel[0].children.length > 0) {
+       directionsPanel[0].innerHTML = compiledDirectionsPanel;
     } else {
-      $('#row').append(compiledDirectionsPanel);
+      directionsPanel.append(compiledDirectionsPanel);
     }
 }
--- cyclesafe/settings.py
+++ cyclesafe/settings.py
@@ -14,17 +14,14 @@ import os.path
 # When running the project locally, some production settings may need to be edited, like project_dir, debug, csrf,
 # staticfiles, etc.
 
-# TODO(debianmaster): May need to set PROJECT_PATH to the absolute path of the project, if Python can't detect it.
-# TODO(debianmaster): You may also need to run the following command, if OpenShift doesn't do it automatically: django-admin.py collectstatic
 # Example PROJECT_DIR: '/var/www/cyclesafe'
 PROJECT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-# TODO(debianmaster): Uncomment CSRF_COOKIE_SECURE once we have https set up.
+# TODO: Uncomment CSRF_COOKIE_SECURE once we have https set up? Not sure if it's needed. It's a Django recommendation.
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # CSRF_COOKIE_SECURE = True
 SESSION_COOKIE_SECURE = True
 
-# TODO (debianmaster): May need to change allowed_hosts to host name. See: https://docs.djangoproject.com/en/1.6/ref/settings/#std:setting-ALLOWED_HOSTS
 ALLOWED_HOSTS = ['*']
 
 ROOT_PATH = os.path.dirname(__file__)
@@ -123,8 +120,6 @@ TEMPLATE_LOADERS = ('django.template.loaders.filesystem.Loader', 'django.templat
 # Media_url specifies the location of user-uploaded files.
 MEDIA_URL = '/static/media/'
 STATIC_URL = '/static/'
-# NOTE: The STATIC_ROOT directory is where staticfiles need to be stored. This may need to be changed depending on
-# OpenShift's requirements.
 STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
 
 STATICFILES_FINDERS = (
